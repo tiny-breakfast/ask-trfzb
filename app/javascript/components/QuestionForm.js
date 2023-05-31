789173edb48de7398a92e536d64bf247af993b3f
x <x>
2023-05-31 14:34:20 -0700

    rename component

--- app/javascript/components/QuestionForm.js
+++ app/javascript/components/QuestionForm.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-class HelloWorld extends React.Component {
+class QuestionForm extends React.Component {
   render () {
     return (
       <React.Fragment>
@@ -10,7 +10,7 @@ class HelloWorld extends React.Component {
   }
 }
 
-HelloWorld.propTypes = {
+QuestionForm.propTypes = {
   greeting: PropTypes.string
 };
-export default HelloWorld
+export default QuestionForm
