e7cbf05d8f2321b661b6e3eb824946cd626a062f
x <x>
2023-05-31 16:51:43 -0700

    replace fragment+greeting with the makings of a form

--- app/javascript/components/QuestionForm.js
+++ app/javascript/components/QuestionForm.js
@@ -1,16 +1,43 @@
 import React from "react"
 import PropTypes from "prop-types"
 class QuestionForm extends React.Component {
+  // pausing the clock at 2:44pm... that's basically 30 minutes from
+  // when I started.
+
+  handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // You can pass formData as a fetch body directly:
+    fetch('/some-api', { method: form.method, body: formData });
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+  }
+
   render () {
     return (
-      <React.Fragment>
-        Greeting: {this.props.greeting}
-      </React.Fragment>
+      // do I need action="/" ? do I want that? I think so - the
+      // QuestionsController will just be the root.
+      <form method="post" onSubmit={this.handleSubmit}>
+        <label>
+          Ask a question: <textarea name="question" id="question">{this.props.question}</textarea>
+        </label>
+
+        <button type="submit" id="ask-button" style={{display: this.props.answer === undefined ? 'block' : 'none' }}>
+          Ask question
+        </button>
+      </form>
     );
   }
 }
 
 QuestionForm.propTypes = {
-  greeting: PropTypes.string
+  answer: PropTypes.string
 };
 export default QuestionForm
