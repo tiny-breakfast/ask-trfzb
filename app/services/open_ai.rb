5ab227a4de205beabfae1f0efe5d753c945ebd63
x <x>
2023-05-31 21:00:38 -0700

    get pretty close to porting this code
    but I haven't run it yet...

--- app/services/open_ai.rb
+++ app/services/open_ai.rb
@@ -1,11 +1,23 @@
+require 'csv'
 require 'openai'
+require 'matrix'
 
 module OpenAI
-    # fixme
-    PAGES_CSV = CSV.new
-    EMBEDDINGS_CSV = CSV.new
+    BOOK = './static/pandp12p.pdf'
+    PAGES_CSV = CSV.read("#{BOOK}.pages.csv")
+    # key is "Page X", value is an 4096 element array of floats
+    EMBEDDINGS =
+        begin
+            # 14M raw on disk.
+            CSV.read("#{BOOK}.embeddings.csv").drop(1).reduce({}) do |embeddings, row|
+                title = row[0]
+                embeddings[title] = row.drop(1)
+                embeddings
+            end
+        end
 
     OPEN_AI_CLIENT = OpenAI::Client.new(access_token: ENV['OPEN_AI_ACCESS_TOKEN'])
+    QUESTION_EMBEDDINGS_MODEL = "text-search-curie-query-001"
     COMPLETIONS_MODEL = "text-davinci-003"
     COMPLETIONS_API_PARAMS = {
         # "We use temperature of 0.0 because it gives the most predictable, factual answer."
@@ -13,6 +25,10 @@ module OpenAI
         max_tokens: 150, # magic number from source
         model: COMPLETIONS_MODEL,
     }.freeze
+
+    MAX_SECTION_LEN = 500
+    SEPARATOR = "\n* "
+    SEPARATOR_LEN = 3
 
     # partially cribbed from Wikipedia, partially from Sahil's header
     HEADER = "Pride and Prejudice is an 1813 novel of manners by Jane Austen. The novel follows the character development of Elizabeth Bennet, the protagonist of the book, who learns about the repercussions of hasty judgments and comes to appreciate the difference between superficial goodness and actual goodness. These are questions and answers about the book. Please keep your answers to three sentences maximum, and speak in complete sentences. Stop speaking once your point is made.\n\nContext that may be useful:\n"
@@ -39,12 +55,12 @@ module OpenAI
     class << self
         def answer_question(question, **options)
             pages_csv = options[:pages_csv] || PAGES_CSV
-            embeddings_csv = options[:embeddings_csv] || EMBEDDINGS_CSV
+            embeddings = options[:embeddings] || EMBEDDINGS
 
             prompt, context = construct_prompt(
                 question,
                 pages_csv,
-                embeddings_csv,
+                embeddings,
             )
 
             response = OPEN_AI_CLIENT.completion(
@@ -60,7 +76,33 @@ module OpenAI
             ]
         end
 
-        def construct_prompt(question, pages_csv, embeddings_csv)
+        def construct_prompt(question, pages_csv, embeddings)
+            most_relevant_document_sections = order_document_sections_by_query_similarity(question, embeddings)
+
+            chosen_sections = []
+            chosen_sections_len = 0
+            # chosen_sections_indexes = []
+
+            most_relevant_document_sections.each do |section_index|
+                document_section = pages_csv.drop(1).select { |row| title = row[0]; title == section_index }
+                content = document_section[1]
+                tokens = document_section[2]
+                
+                chosen_sections_len += tokens + SEPARATOR_LEN
+
+                if chosen_sections_len > MAX_SECTION_LEN
+                    space_left = MAX_SECTION_LEN - chosen_sections_len - SEPARATOR.length
+                    chosen_sections << (SEPARATOR + content[0...space_left])
+                    # chosen_sections_indexes << str(section_index)
+                    break
+                end
+
+                chosen_sections << (SEPARATOR + content)
+                # chosen_sections_indexes << section_index
+            end
+
+            context = chosen_sections.join("")
+
             prompt = header +
                 context +
                 QUESTIONS_AND_ANSWERS.take(10).map { |q, a| "\n\n\nQ: #{q}\n\nA: #{a}" } +
@@ -70,6 +112,33 @@ module OpenAI
                 prompt,
                 context,
             ]
+        end
+
+        # Find the query embedding for the supplied query, and compare it against all of the pre-calculated document embeddings
+        # to find the most relevant sections.
+        # 
+        # Return the list of document sections, sorted by relevance in descending order.
+        def order_document_sections_by_query_similarity(question, embeddings) # -> list[(float, (str, str))]:
+            question_embedding = get_question_embedding(question)
+
+            return embeddings.map do |doc_index, doc_embedding|
+                vector_similarity = Vector[*question_embedding].dot(Vector[*doc_embedding])
+                [
+                    vector_similarity,
+                    doc_index,
+                ]
+            end.sort.reverse
+        end
+
+        def get_question_embedding(text)
+            result = OPEN_AI_CLIENT.embeddings(
+                parameters: {
+                    model: QUESTION_EMBEDDINGS_MODEL,
+                    input: text,
+                },
+            )
+
+            return result["data"][0]["embedding"]
         end
     end
 end
