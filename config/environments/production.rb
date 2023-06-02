e6994678e5be198192b12b66a69b7918347ccb28
x <x>
2023-06-02 13:10:39 -0700

    update app name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "ask_pride_and_prejudice_production"
+  # config.active_job.queue_name_prefix = "ask_the_real_frank_zappa_book_production"
 
   config.action_mailer.perform_caching = false
 
