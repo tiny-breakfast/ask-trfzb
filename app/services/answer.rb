4b46fec9b37e9c58ef7e1d246c21d38e1cbd3747
x <x>
2023-06-02 12:04:42 -0700

    some more questions

--- app/services/answer.rb
+++ app/services/answer.rb
@@ -44,10 +44,24 @@ module Answer
     # wholly taken from https://www.sparknotes.com/lit/pride/key-questions-and-answers/
     QUESTIONS_AND_ANSWERS = {
         "What was Frank Zappa's biggest problem with school?" => "My biggest problem, throughout school, was that the things they were trying to teach me tended not to be the kinds of things I was interested in. I grew up with poison gas and explosives -- with the children of people who built these things for a living. Did I give a fuck about algebra?",
+
         "Did Frank Zappa read Shakespeare?" => "The epigraphs at the heads of chapters were researched and inserted by Peter -- I mention this because I wouldn't want anybody to think I sat around reading Flaubert, Twitchell and Shakespeare all day.",
+
         "Why did Frank Zappa write this book?" => "One of the reasons for doing this is the proliferation of stupid books (in several languages) which purport to be About Me. I thought there ought to be at least ONE, somewhere, that had real stuff in it. The opportunity to say stuff in print about tangential subjects is appealing.",
+
         "How did teenaged Frank Zappa treat visitors to his house?" => "All through high school, whenever people came over, I would force them to listen to Varèse -- because I thought it was the ultimate test of their intelligence.",
-        
+
+        "Should I quit school or go on?" => "I always tell them you should infiltrate. Hurry up and take over your father's job and do it right. It's fortunate that something like Psychedelia and Haight-Ashbury and all the rest of that goes on because it takes the focus off of the possibility that somebody who looks clean, straight, wholesome, right-wing and harmless is going to come in there and just do it while they're not looking. It's a much more frightening concept thinking it's some hairy creep that's going to take over your job. It's not going to be that way, it's going to be some guy who is straight.",
+
+        "Is music better or worse than when you started? And I don’t mean your music I mean music in general." => "Well if you’re talking about the known musical universe - in other words, what you can hear on the radio and what they show you on MTV - it is way worse. But that doesn’t mean that there aren’t good things out there that we don’t know about. It’s just that the broadcasters are not letting us find out about it. Because it’s hard for me to believe that all the sudden with the advent of MTV all good songs ceased to be written, all good bands ceased to be formed. I just don’t think that nature works that way. In some place there’s good musicians and good composers and good tunes all over this country and other countries, we just don’t know about them because the people who determine what you get to see and hear have no taste."
+
+        "Do you enjoy touring?" => "If I had to choose I'd be touring. I like to have something happening where the music is alive, and there's people in it, and some feeling to it. It's so hard to get something that exciting in a studio. I think that anyone that wants to shut himself up in a studio for the rest of his life is missing out. To me, the studio is a useful tool. It's a great place to do certain experimental things. There are a number of things that are feasible in the studio that are impossible on a stage, like overdubbing. But for getting out and 'doing it' you've got to go on the road.",
+
+        "Are you a rebel without a cause?" => "Hardly, because my cause is music. I'm interested in ﬁnding out what can be done with different types of musical forms of expression – without any interference. It's very difﬁcult to do in the United States because, unless you can sell it to somebody, you can't keep doing it.,"
+
+        "What is the concept of 'the Big Note'?" => "The concept of the Big Note is that everything in the universe is composed basically of vibrations – light is a vibration, sound is a vibration, atoms are composed of vibrations – and all these vibrations just might be harmonics of some incomprehensible fundamental cosmic tone.",
+
+        "Would you say that music is your whole life?" => "Yes, just about.",
     }
 
     class << self
