479fecb771c8a86d0b10e19f7453d03f1af650e6
x <x>
2023-06-06 19:08:50 -0700

    write more tests

--- spec/system/questions_forms_spec.rb
+++ spec/system/questions_forms_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 require 'webdrivers'
 
-
 RSpec.describe "question form", type: :system do
     include Rails.application.routes.url_helpers
 
@@ -9,9 +8,51 @@ RSpec.describe "question form", type: :system do
         default_url_options[:host] = "localhost"
     end
 
-    it "visiting the index" do
-        visit root_url
-        
-        expect(page).to have_content('Ask "the Real Frank Zappa Book"')
+    before do
+        allow(Answer).
+            to receive(:answer_question).
+            with("What's totally bitchen in Encino?").
+            and_return([
+                "There's, like, the Galleria, and, like, all these, like, really great shoe stores.",
+                "some kind of context",
+            ])
+    end
+
+    describe "visiting the index" do
+        it "shows the form and then shows the answer" do
+            visit root_url
+
+            expect(page).to have_content('Ask "the Real Frank Zappa Book"')
+            within("#question-form") do
+                fill_in 'Ask a question', with: "What's totally bitchen in Encino?"
+            end
+
+            click_button('Ask question')
+            sleep(30.seconds)
+
+            expect(page).to have_content("There's, like, the Galleria, and, like, all these, like, really great shoe stores.")
+            expect(page).to have_content("Ask another question")
+        end
+    end
+
+    describe "viewing a particular question and answer" do
+        let!(:question) do
+            Question.create!(
+                :question => "some question",
+                :answer => "some answer",
+            )
+        end
+
+        it "shows the form and then shows the answer" do
+            visit question_url(:id => question.id)
+
+            expect(page).to have_content('Ask "the Real Frank Zappa Book"')
+            expect(page).to_not have_content("Ask question")
+            within("#question-form") do
+                expect(page).to have_content("some question")
+            end
+
+            expect(page).to have_content("some answer")
+        end
     end
 end
